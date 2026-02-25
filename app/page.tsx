00fd3682fad8e44f4a13967bbef5cba928ddb4bb
Shruthilaya Jindamwar <jindamwarshruthilaya@gmail.com>
2026-02-25 06:14:55 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 export default function Home() {
+  const arr = [
+    "buy vegetables", //
+    "buy books",
+    "buy pens",
+    "buy pencils",
+    "buy tv",
+  ];
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>
           Task list
           <ul>
-            <li>. buy vegetables</li>
-            <li>. buy books</li>
+            {arr.map((item) => {
+              return <li key={item}># {item} </li>;
+            })}
           </ul>
         </div>
       </main>
