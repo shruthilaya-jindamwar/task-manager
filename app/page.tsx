a2ddfa6a85e34fe403d54c5b0a03344a273eac0c
Shruthilaya Jindamwar <jindamwarshruthilaya@gmail.com>
2026-02-25 06:33:24 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
-  const arr = [
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  const [arr, setArr] = useState([
     "buy vegetables", //
     "buy books",
     "buy pens",
     "buy pencils",
     "buy tv",
-  ];
+  ]);
+
+  const onNewTaskChange = (e) => {
+    setNewTaskTitle(e.target.value);
+  };
+
+  const onClickAdd = () => {
+    setArr([
+      ...arr,
+     newTaskTitle
+    ]);
+
+    setNewTaskTitle("")
+  };
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+
+
         <div>
           Task list
           <ul>
@@ -17,6 +38,17 @@ export default function Home() {
               return <li key={item}># {item} </li>;
             })}
           </ul>
+        </div>
+
+        <div>
+          Enter a task title:
+          <input
+            onChange={onNewTaskChange}
+            className="border-2 border-amber-50"
+          ></input>
+          <button onClick={onClickAdd} className="border-2 border-amber-500">
+            Add
+          </button>
         </div>
       </main>
     </div>
